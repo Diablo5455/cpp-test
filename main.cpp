818d90051e9713c3c158f620bd8d1adbabcfd08e
Irina Kuchishkina <Diablobik33@gmail.com>
2019-05-26 13:09:11 +0400

    Calculator aded

--- main.cpp
+++ main.cpp
@@ -1,57 +1,98 @@
 #include <iostream>
 #include <cstdlib>
-using namespace std;
+#include <string>
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int minus(int a, int b) {
+	return a - b;
+}
+
+int multiply(int a, int b) {
+	return a * b;
+}
+
+int divide(int a, int b) {
+	return a / b;
+}
+
+int read_num() {
+	int num;
+	std::cout << "Input number: ";
+	std::cin >> num;
+	return num;
+}
+
+int op_minus() {
+
+	int b, c;
+	b = read_num();
+	c = read_num();
+	int d = minus(b, c);
+	std::cout << "sum: " << d << std::endl;
+	return d;
+}
+
+int op_add() {
+	int b, c;
+	b = read_num();
+	c = read_num();
+	int d = add(b, c);
+	std::cout << "Result: " << d << std::endl;
+	return d;
+}
+
+int op_multiply() {
+
+	int b, c;
+	b = read_num();
+	c = read_num();
+	int d = multiply(b, c);
+	std::cout << "sum: " << d << std::endl;
+	return d;
+}
+
+
+int op_divide() {
+
+	int b, c;
+	b = read_num();
+	c = read_num();
+	int d = divide(b, c);
+	std::cout << "sum: " << d << std::endl;
+	return d;
+}
+
+
 
 int main()
 {
 	setlocale(0,"");
 	int a;
-	cout << "World" << endl;
-	cout << "1 = +" << endl;
-	cout << "2 = -" << endl;
-	cout << "3 = *" << endl;
-	cout << "4 = /" << endl;
-	cin >> a;
+	std::cout << "World" << std::endl;
+	std::cout << "1 = +" << std::endl;
+	std::cout << "2 = -" << std::endl;
+	std::cout << "3 = *" << std::endl;
+	std::cout << "4 = /" << std::endl;
+	std::cin >> a;
 	if ( a <= 1) {
-	int b, c;
-	cout << "num 1";
-	cin >> b;
-	cout << "num 2";
-	cin >> c;
-	int d = b + c;
-	cout << "sum: " << d << endl;
+		op_add();
 	} else if ( a <= 2) {
-	int b, c;
-	cout << "num 1";
-	cin >> b;
-	cout << "num 2";
-	cin >> c;
-	int d = b - c;
-	cout << "sum: " << d << endl;
+		op_minus();
 	} else if ( a <= 3) {
-	int b, c;
-	cout << "num 1";
-	cin >> b;
-	cout << "num 2";
-	cin >> c;
-	int d = b * c;
-	cout << "sum: " << d << endl;
+		op_multiply();
 	} else if ( a <= 4) {
-	int b, c;
-	cout << "num 1";
-	cin >> b;
-	cout << "num 2";
-	cin >> c;
-	int d = b + c;
-	cout << "sum: " << d << endl;
+		op_divide();
 	} {
 	setlocale (0, "");
 	double d;
 	int f = d / 2;
 	if (d < 2){
-	cout << "yes" << endl;
+	std::cout << "yes" << std::endl;
 	} else {
-	cout << "no" << endl;
+	std::cout << "no" << std::endl;
 	}
 	}
 	return 0;
